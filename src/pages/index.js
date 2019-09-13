af9f6e8776f26ab5c754ff8bc2074299235ff254
Dan Choiniere <dchoinie@gmail.com>
2019-09-13 14:56:55 -0500

    Delete Stripe failing code

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,34 +5,11 @@ import Hero from "../components/hero"
 import Features from "../components/features"
 
 const IndexPage = () => {
-  // const stripe = window.Stripe("pk_test_8xiHw0K0PYiSDC2r0LnjdnKu00h00lezGz")
-
-  // const placeOrder = sku => {
-  //   stripe.redirectToCheckout({
-  //     items: [
-  //       {
-  //         sku,
-  //         quantity: 1,
-  //       },
-  //     ],
-  //     successUrl: "http://localhost:8000/success",
-  //     cancelUrl: "http://localhost:8000/cancel",
-  //   })
-  // }
-
   return (
     <Layout>
       <SEO title="Home" />
       <Hero />
       <Features />
-      {/* <div>
-        <article>
-          <h3>Single Day Pass</h3>
-          <button style={{}} onClick={() => placeOrder("sku_FbzYNZNZbE2Xr0")}>
-            Purchase
-          </button>
-        </article>
-      </div> */}
     </Layout>
   )
 }
